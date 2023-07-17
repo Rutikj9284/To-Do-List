621e616d9550caeae5d9fa3ff857c5e814221716
Rutik Jaybhaye <jaybhayerutik2@gmail.com>
2023-07-17 16:59:01 +0530

    first commit

--- app.js
+++ app.js
@@ -134,8 +134,8 @@ app.post("/delete", function (req, res) {
 // })
 
 
-app.listen(3000, function () {
-  console.log("3000");
+app.listen(5000, function () {
+  console.log("5000");
 });
 
 // Item.insertMany(arr
